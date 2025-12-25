648989d11ba25677f3bb32dc828b45da97f61832
UnidentifiedX <80146445+UnidentifiedX@users.noreply.github.com>
2025-12-26 02:35:01 +0800

    Refactor temporary_evaluate_objective to accept x_range and y_range parameters and added a ring radius optimiser script

--- objective_function.py
+++ objective_function.py
@@ -60,9 +60,9 @@ def evaluate_objective(detector_regions: list[utils.DetectorRegion],
 
     return total_energy
 
-def temporary_evaluate_objective(input_dir):
-    data = detector.detect_waves(x_range=(175, 176), 
-        y_range=(0, 20), 
+def temporary_evaluate_objective(input_dir, x_range=(175, 176), y_range=(0, 20)):
+    data = detector.detect_waves(x_range=x_range, 
+        y_range=y_range, 
         input_dir=input_dir,
         frame_count=len([name for name in os.listdir(input_dir) if re.fullmatch(rf"m_full\d+\.npy", name)]),
         dt=50e-12,
